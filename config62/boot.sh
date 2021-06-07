3e70c113647e278e1edd418c4ed530a21773472c
linuszhanglei <linuszhanglei@didiglobal.com>
2021-06-07 15:19:19 +0800

    fix part m2

--- config62/boot.sh
+++ config62/boot.sh
@@ -12,11 +12,13 @@ echo "进入swarm目录执行deploy shell"
 mkdir -p /root/deploy
 cp /root/swarm/config62/deploy_step_part.sh /root/deploy/deploy.sh
 cp /root/swarm/config62/deploy_part.sh /root/deploy/deploy_part.sh
+cp /root/swarm/config62/deploy_part_m2.sh /root/deploy/deploy_part_m2.sh
 cp /root/swarm/config62/deploy_env.sh /root/deploy/deploy_env.sh
 cp /root/swarm/config62/deploy_clef.sh /root/deploy/deploy_clef.sh
 cp /root/swarm/config62/deploy_bee.sh /root/deploy/deploy_bee.sh
 chmod a+x /root/deploy/deploy.sh
 chmod a+x /root/deploy/deploy_part.sh
+chmod a+x /root/deploy/deploy_part_m2.sh
 chmod a+x /root/deploy/deploy_env.sh
 chmod a+x /root/deploy/deploy_clef.sh
 chmod a+x /root/deploy/deploy_bee.sh
--- config62/deploy_part_m2.sh
+++ config62/deploy_part_m2.sh
@@ -3,8 +3,6 @@
 set -euxo pipefail
 
 if [ -d "/var/lib/docker" ]; then 
-    LVM_INFO = ($(blkid | grep "TLVM"))
-    if [ -z "$LVM_INFO" ]; then 
         yum install -y lvm2
         pvcreate  /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -y
         vgcreate TLVM  /dev/nvme0n1 
@@ -16,9 +14,6 @@ if [ -d "/var/lib/docker" ]; then
         echo "Local disks are provisioned succesfully"
         echo "=============== 重置docker默认存储路径  /var/lib/docker 重启docker =================="
         sudo systemctl restart docker
-    else 
-        echo "已经完成挂在默认路径 /var/lib/docker"
-    fi
 
 else 
     echo "默认存储路径/var/lib/docker不存在"
